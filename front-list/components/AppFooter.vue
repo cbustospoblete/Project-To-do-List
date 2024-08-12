a82a10f5d99c0e0934167fc942077767e6639960
Carlos Bustos <104233390+cbustospoblete@users.noreply.github.com>
2024-08-12 02:57:18 -0400

    Delete front-list directory

--- front-list/components/AppFooter.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <footer class="bg-[#001529] text-white py-4">
-    <div class="container mx-auto flex justify-between items-center">
-      <p class="text-sm">© 2024 To-do List. Todos los derechos reservados.</p>
-    </div>
-  </footer>
-</template>
--- front-list/layouts/default.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="flex flex-col min-h-screen">
-    <AppHeader />
-    <main class="flex-1">
-      <slot />
-    </main>
-    <AppFooter />
-  </div>
-</template>
